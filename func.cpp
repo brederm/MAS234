1d968a6bc10bb25fdfc1042e72b692b687d23edd
brederm <breder18@uia.no>
2020-10-27 14:55:05 +0100

    master

---
+++ func.cpp
@@ -0,0 +1,14 @@
+// function calc
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+const double a = 2.0;
+const double b = 5.0;
+double f = a * b * b;
+std::cout <<"Svaret er f="<< f << std::endl;
+
+return 0;
+}
